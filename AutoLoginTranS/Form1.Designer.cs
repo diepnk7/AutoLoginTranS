2f94934a9fbb3c2c9c86aabdd86cc08b7bb5d968
Nguyễn Ngọc Diệp <yoyaldev@gmail.com>
2020-04-24 17:46:29 +0700

    Upload add sourcecode

---
+++ AutoLoginTranS/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace AutoLoginTranS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbHour = new System.Windows.Forms.ComboBox();
+            this.cbMinute = new System.Windows.Forms.ComboBox();
+            this.labelTagetTime = new System.Windows.Forms.Label();
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLazy = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbHour
+            // 
+            this.cbHour.FormattingEnabled = true;
+            this.cbHour.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.cbHour.Location = new System.Drawing.Point(93, 12);
+            this.cbHour.Name = "cbHour";
+            this.cbHour.Size = new System.Drawing.Size(43, 21);
+            this.cbHour.TabIndex = 0;
+            this.cbHour.Text = "00";
+            // 
+            // cbMinute
+            // 
+            this.cbMinute.FormattingEnabled = true;
+            this.cbMinute.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.cbMinute.Location = new System.Drawing.Point(142, 12);
+            this.cbMinute.Name = "cbMinute";
+            this.cbMinute.Size = new System.Drawing.Size(43, 21);
+            this.cbMinute.TabIndex = 1;
+            this.cbMinute.Text = "00";
+            // 
+            // labelTagetTime
+            // 
+            this.labelTagetTime.AutoSize = true;
+            this.labelTagetTime.Location = new System.Drawing.Point(26, 15);
+            this.labelTagetTime.Name = "labelTagetTime";
+            this.labelTagetTime.Size = new System.Drawing.Size(64, 13);
+            this.labelTagetTime.TabIndex = 2;
+            this.labelTagetTime.Text = "Taget Time:";
+            // 
+            // txtBoxID
+            // 
+            this.txtBoxID.Location = new System.Drawing.Point(248, 12);
+            this.txtBoxID.Name = "txtBoxID";
+            this.txtBoxID.Size = new System.Drawing.Size(43, 20);
+            this.txtBoxID.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID:";
+            // 
+            // btnLazy
+            // 
+            this.btnLazy.Location = new System.Drawing.Point(125, 55);
+            this.btnLazy.Name = "btnLazy";
+            this.btnLazy.Size = new System.Drawing.Size(80, 28);
+            this.btnLazy.TabIndex = 5;
+            this.btnLazy.Text = "Lazy now!";
+            this.btnLazy.UseVisualStyleBackColor = true;
+            this.btnLazy.Click += new System.EventHandler(this.btnLazy_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 98);
+            this.Controls.Add(this.btnLazy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxID);
+            this.Controls.Add(this.labelTagetTime);
+            this.Controls.Add(this.cbMinute);
+            this.Controls.Add(this.cbHour);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoLoginTranS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbHour;
+        private System.Windows.Forms.ComboBox cbMinute;
+        private System.Windows.Forms.Label labelTagetTime;
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLazy;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ AutoLoginTranS/Form1.cs
@@ -0,0 +1,120 @@
+﻿using KAutoHelper;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace AutoLoginTranS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLazy_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            WindowState = FormWindowState.Minimized;
+        }
+
+        #region LoginTrans
+
+        public void LoginTranS()
+        {
+            string strCmdText;
+            strCmdText = @"/C C:\Users\yoyal\AppData\Roaming\TranS\TranS.exe";
+
+            Process p = new Process();
+
+            p.StartInfo.FileName = "CMD.exe";
+            p.StartInfo.Arguments = strCmdText;
+
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.Start();
+
+            Thread.Sleep(5000);
+
+            //click Student
+            var screen = CaptureHelper.CaptureScreen();
+            screen.Save("mainScreen.png");
+            var subBitmap = ImageScanOpenCV.GetImage("hocVien.bmp");
+            var resBitmap = ImageScanOpenCV.FindOutPoint((Bitmap)screen, subBitmap);
+            if (resBitmap != null)
+            {
+                string x = (resBitmap.ToString()).Remove(0, 3).Remove(3, 7);
+                string y = (resBitmap.ToString()).Remove(0, 9).Remove(3, 1);
+                AutoControl.MouseClick(int.Parse(x) + 20, int.Parse(y) + 20, EMouseKey.LEFT);
+            }
+
+            Thread.Sleep(500);
+
+            //Send Enter ID
+            try
+            {
+                AutoControl.SendStringFocus(txtBoxID.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bạn chưa nhập ID");
+            }
+            
+
+            //Click Join now
+            var screenID = CaptureHelper.CaptureScreen();
+            screenID.Save("mainScreen.png");
+            var subBitmapID = ImageScanOpenCV.GetImage("btnJoinnow.bmp");
+            var resBitmapID = ImageScanOpenCV.FindOutPoint((Bitmap)screenID, subBitmapID);
+            if (resBitmapID != null)
+            {
+                string x1 = (resBitmapID.ToString()).Remove(0, 3).Remove(3, 7);
+                string y1 = (resBitmapID.ToString()).Remove(0, 9).Remove(3, 1);
+                AutoControl.MouseClick(int.Parse(x1) + 20, int.Parse(y1) + 20, EMouseKey.LEFT);
+            }
+            Application.Exit();
+        }
+        #endregion //LoginTranS
+
+        #region CheckTime
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            string strHour = null;
+
+            if (cbHour.Text != "")
+            {
+
+                if (cbMinute.Text != "")
+                {
+                    strHour = cbHour.Text + ":" + cbMinute.Text + "";
+                }
+                else
+                {
+                    strHour = cbHour.Text + ":" + "00" + "";
+                }
+
+                //Lấy Giờ Hệ Thống
+
+                string strSystemHour = DateTime.Now.ToShortTimeString();
+
+                if (strHour == strSystemHour)
+                {
+                    timer.Stop();
+                    LoginTranS();
+                    //AutoLogin.LoginTranS(txtBoxID.Text.ToString());
+                }
+                //DateTime someFutureTime = new DateTime();
+                //someFutureTime = ConvertDateTime();
+                //AlarmClock clock = new AlarmClock(someFutureTime);
+                //clock.Alarm += (sender, e) => MessageBox.Show("Wake up!");
+            }
+        }
+        #endregion //CheckTime
+
+    }
+}
